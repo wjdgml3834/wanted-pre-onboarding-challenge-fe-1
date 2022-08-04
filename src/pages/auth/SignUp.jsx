ad718e1a1d620fd06e7d1ded17a915d6a5a1ad8c
Your Name <wjdgml3834@naver.com>
2022-08-04 12:40:26 +0900

    ✨Feat: add userInputs feature in SignUp pages

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,10 +1,36 @@
+import { useCallback, useState } from "react";
+
 function SignUp() {
+  const [userInputs, setUserInputs] = useState({
+    email: "",
+    password: "",
+  });
+  const handleInput = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setUserInputs({
+        ...userInputs,
+        [name]: value,
+      });
+    },
+    [userInputs]
+  );
   return (
     <form>
-      <label for="email">이메일</label>
-      <input type="email" id="email" />
-      <label for="password">비밀번호</label>
-      <input type="password" id="password" />
+      <input
+        name="email"
+        type="email"
+        id="email"
+        onChange={handleInput}
+        placeholder="이메일을 입력해주세요"
+      />
+      <input
+        name="password"
+        type="password"
+        id="password"
+        onChange={handleInput}
+        placeholder="비밀번호를 입력해주세요"
+      />
       <button type="submit">회원가입</button>
     </form>
   );
