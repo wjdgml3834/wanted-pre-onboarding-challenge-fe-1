687314effad6f7f2673faad52db4e287b36778f5
Your Name <wjdgml3834@naver.com>
2022-08-04 14:42:38 +0900

    ✨Feat: connect api in SignUp page

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,4 +1,5 @@
 import { useCallback, useState } from "react";
+import axios from "axios";
 
 function SignUp() {
   const [userInputs, setUserInputs] = useState({
@@ -15,23 +16,37 @@ function SignUp() {
     },
     [userInputs]
   );
+
+  const createUser = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8080/users/create", {
+        email: userInputs.email,
+        password: userInputs.password,
+      });
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <form>
       <input
         name="email"
         type="email"
-        id="email"
         onChange={handleInput}
         placeholder="이메일을 입력해주세요"
       />
       <input
         name="password"
         type="password"
-        id="password"
         onChange={handleInput}
         placeholder="비밀번호를 입력해주세요"
       />
-      <button type="submit">회원가입</button>
+      <button type="submit" onClick={createUser}>
+        회원가입
+      </button>
     </form>
   );
 }
