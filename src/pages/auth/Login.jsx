74951676720511f6176518ad9cdea44e47006eb5
Your Name <wjdgml3834@naver.com>
2022-08-04 11:22:36 +0900

    💄Design: change submit button name to login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -5,7 +5,7 @@ function Login() {
       <input type="email" id="email" />
       <label for="password">비밀번호</label>
       <input type="password" id="password" />
-      <button type="submit">제출</button>
+      <button type="submit">로그인</button>
     </form>
   );
 }
