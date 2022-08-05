fc11368ac5ba0040248253e0e1bfa40dcc00112e
Your Name <wjdgml3834@naver.com>
2022-08-05 11:34:47 +0900

    ✨Feat: add signup validation in SignUp page

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -6,6 +6,15 @@ function SignUp() {
     email: "",
     password: "",
   });
+
+  //오류메시지 상태 검사
+  const [emailMessage, setEmailMessage] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
+
+  //유효성 검사
+  const [isEmail, setIsEmail] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
+
   const handleInput = useCallback(
     (e) => {
       const { name, value } = e.target;
@@ -13,6 +22,29 @@ function SignUp() {
         ...userInputs,
         [name]: value,
       });
+      if (name === "email") {
+        const emailRegex =
+          /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        if (!emailRegex.test(value)) {
+          setEmailMessage("이메일 형식이 틀렸습니다.");
+          setIsEmail(false);
+        } else {
+          setEmailMessage("올바른 형식입니다.");
+          setIsEmail(true);
+        }
+      } else if (name === "password") {
+        const passwordRegex =
+          /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+        if (!passwordRegex.test(value)) {
+          setPasswordMessage(
+            "숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!"
+          );
+          setIsPassword(false);
+        } else {
+          setPasswordMessage("안전한 비밀번호입니다.");
+          setIsPassword(true);
+        }
+      }
     },
     [userInputs]
   );
@@ -29,7 +61,6 @@ function SignUp() {
       console.log(e);
     }
   };
-
   return (
     <form>
       <input
@@ -38,13 +69,19 @@ function SignUp() {
         onChange={handleInput}
         placeholder="이메일을 입력해주세요"
       />
+      {userInputs.email.length > 0 && <span>{emailMessage}</span>}
       <input
         name="password"
         type="password"
         onChange={handleInput}
         placeholder="비밀번호를 입력해주세요"
-      />
-      <button type="submit" onClick={createUser}>
+      />{" "}
+      {userInputs.password.length > 0 && <span>{passwordMessage}</span>}
+      <button
+        type="submit"
+        onClick={createUser}
+        disabled={!(isEmail && isPassword)}
+      >
         회원가입
       </button>
     </form>
