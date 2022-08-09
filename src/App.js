6893a42b4e9f32f2f8109aeaee66d4c4a729a259
Your Name <wjdgml3834@naver.com>
2022-08-09 15:59:33 +0900

    ✨Feat: connect api in Login page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <SignUp />
+      <Login />
     </div>
   );
 }
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,11 +1,52 @@
+import { useCallback, useState } from "react";
+import axios from "axios";
+
 function Login() {
+  const [userInputs, setUserInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setUserInputs({
+        ...userInputs,
+        [name]: value,
+      });
+    },
+    [userInputs]
+  );
+  const loginUser = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8080/users/login", {
+        email: userInputs.email,
+        password: userInputs.password,
+      });
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <form>
-      <label for="email">이메일</label>
-      <input type="email" id="email" />
-      <label for="password">비밀번호</label>
-      <input type="password" id="password" />
-      <button type="submit">로그인</button>
+      <input
+        name="email"
+        type="email"
+        onChange={handleInput}
+        placeholder="이메일을 입력해주세요"
+      />
+      <input
+        name="password"
+        type="password"
+        onChange={handleInput}
+        placeholder="비밀번호를 입력해주세요"
+      />
+      <button type="submit" onClick={loginUser}>
+        로그인
+      </button>
     </form>
   );
 }
